5a00487ba3d87973e72b9a81d6ab8f0e3f02ce43
Francisco-Gomera <225b9312@live.uem.es>
2026-08-19 15:57:32 -0400

    Fix BrowserRouter, Routes, Route import from react-router-dom in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import { AuthProvider } from './context/AuthContext';
 import { PageLayout } from './components/layout/PageLayout';
 import { DashboardLayout } from './components/layout/DashboardLayout';
